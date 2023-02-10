fd341b906b69f726a80309a332c67ee85efb7048
Princess Bethea (pbethea) <pbethea@cisco.com>
2023-02-10 16:33:18 -0500

    seed file

---
+++ seed.js
@@ -0,0 +1,17 @@
+const {items} = require("./seedData.js");
+const {sequelize} = require("./db");
+const {Item} = require("./models");
+
+const seed = async () => {
+    try {
+        await sequelize.sync ({ force: true});
+        await Promise.all (items.map(item => Item.create (item)));
+        console.log("db populated");
+    } catch(error){ 
+        console.error(error)
+    }
+}
+
+seed();
+
+module.export = seed
