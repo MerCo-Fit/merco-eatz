3cedc8769c2d124e172058edc1a471c07d7ebbe1
Halina Cox <halina.cox@halina.cox-XPHWNQNK73>
2023-02-24 10:33:54 -0800

    MVP

--- routes/items.js
+++ routes/items.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { Item } = require('../models');
+// const { sequelize } = require('./db')
+// const { items } = require('../seedData')
 
-
-//const { sequelize } = require('./db')
-//const { items } = require('../seedData')
-
-// router.use(express.json())
-// router.use(express.urlencoded({
-//     extended: true
-//   }))    
+ router.use(express.json())
+ router.use(express.urlencoded({
+    extended: true
+   }))    
   
-  router.get('/', async (req, res, next) => {
-    try {
-      //const meal = req.query.meal;
-        //const where = {};
-        //if(meal)
-        //where.meal = meal;
+   router.get('/', async (req, res, next) => {
+     try {
+       const meal = req.query.meal;
+        const where = {};
+        if(meal)
+        where.meal = meal;
       const items = await Item.findAll();
       res.send(items);
     } catch (error) {
@@ -25,48 +23,57 @@ const { Item } = require('../models');
   });
 
 
-// ADD ITEM (CREATE)
-// router.post('/', async (req, res, next) => {
-//   try {
-//     const addItem = await Item.create(req.body)  
-//     res.send(addItem)
-//   } catch (error) {
-//     next(error)  
-//   }
-// })
+//ADD ITEM (CREATE)
+router.post('/', async (req, res, next) => {
+  try {
+    const addItem = await Item.create(req.body)  
+    res.send(addItem)
+  } catch (error) {
+    next(error)  
+  }
+})
 
-// DELETE ITEM
-// router.delete('/:id', async (req, res, next) =>{
-//   try {
-//     const deletedItem = await Item.findByPk(req.params.id)  
-//      await deletedItem.destroy()
-//     res.send(await Item.findAll())
-//   } catch (error) {
-//     next(error)  
-//   }
-// })
+//DELETE ITEM
+router.delete('/:id', async (req, res, next) =>{
+  try {
+    const deletedItem = await Item.findByPk(req.params.id)  
+     await deletedItem.destroy()
+    res.send(await Item.findAll())
+  } catch (error) {
+    next(error)  
+  }
+})
 
-// GET ITEMS (READ)
-// GET SINGLE ITEM (READ)
-// router.get('/:id', async (req, res, next) => {
-//   try {
-//     const items = await Item.findByPk(req.params.id)
-//     res.send(items)
-//   } catch (error) {
-//     next(error)
-//   }
-// })
+//GET ITEMS (READ)
+router.get('/', async (req, res, next) => {
+  try{
+    const items = await Item.findAll()
+    res.send(items)
+  } catch (error) {
 
-// EDIT ITEM (UPDATE/PUT)
-// router.put('/:id', async (req, res, next) => {
-//   try{
-//     const updatedItem = await Item.update(req.body, 
-//       {where: {id: req.params.id}})
-//     res.send(await Item.findAll())
-//   } catch (error){
-//     next(error)
-//   }
-// })
+  }
+})
 
-module.exports = router;
+//GET SINGLE ITEM (READ)
+router.get('/:id', async (req, res, next) => {
+  try {
+    const item = await Item.findByPk(req.params.id)
+    res.send(item)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//EDIT ITEM (UPDATE/PUT)
+router.put('/:id', async (req, res, next) => {
+  try{
+    const updatedItem = await Item.update(req.body, 
+      {where: {id: req.params.id}})
+    res.send(await Item.findAll())
+  } catch (error){
+    next(error)
+  }
+})
+
+module.exports = router
 
